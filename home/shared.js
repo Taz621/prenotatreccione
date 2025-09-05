c133df52236c90ddf8df704dd2860725b1b0ae3e
Taz621 <marco.topazio621@gmail.com>
2025-09-05 12:07:47 +0200

    Update shared.js

--- home/shared.js
+++ home/shared.js
@@ -1,31 +1,17 @@
 // home/shared.js
 
 const themeToggleButton = document.getElementById('theme-toggle');
-const docElement = document.documentElement; // Usiamo l'elemento <html>
-
-// Funzione per aggiornare l'icona del pulsante al caricamento
-const updateButtonIcon = () => {
-    if (docElement.classList.contains('dark-mode')) {
-        themeToggleButton.textContent = '☀️';
-    } else {
-        themeToggleButton.textContent = '🌙';
-    }
-};
+const docElement = document.documentElement;
 
 // Aggiungi l'evento al click del pulsante
 themeToggleButton.addEventListener('click', () => {
-    // Alterna la classe dark-mode
+    // Alterna la classe sul tag <html>
     docElement.classList.toggle('dark-mode');
 
-    // Salva la nuova preferenza e aggiorna l'icona
+    // Salva la nuova preferenza in base allo stato attuale
     if (docElement.classList.contains('dark-mode')) {
         localStorage.setItem('theme', 'dark');
-        themeToggleButton.textContent = '☀️';
     } else {
         localStorage.setItem('theme', 'light');
-        themeToggleButton.textContent = '🌙';
     }
 });
-
-// Aggiorna l'icona non appena il DOM è pronto
-document.addEventListener('DOMContentLoaded', updateButtonIcon);
